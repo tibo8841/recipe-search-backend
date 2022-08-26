ac4d4258344dd333bcc6f9ad9b0634c32c609900
tibo8841 <thibaut.hucker@gmail.com>
2022-08-26 16:50:42 +0100

    Setting up environment variables for port and DB connection

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
-const port = 8080;
+const { Client } = require("pg");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+const connectionString = process.env.CONNECTION_STRING;
+
+const client = new Client(connectionString);
+client.connect();
 
 app.get("/", (req, res) => {
   res.send("Testing server works");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
