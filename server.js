6ec5bfe8b213631b875d181a0d3263479589c46e
tibo8841 <thibaut.hucker@gmail.com>
2022-08-29 18:39:47 +0100

    writing some of the endpoints

--- server.js
+++ server.js
@@ -11,9 +11,68 @@ const client = new Client(connectionString);
 client.connect();
 
 app.get("/", (req, res) => {
-  res.send("Testing server works");
+  res.send("Recipe Search");
+});
+
+app.get("/test", (req, res) => {
+  res.send("testing server works");
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+app.post("/register", async (req, res) => {
+  await registerUser(req, res);
+});
+
+app.get("/login", async (req, res) => {
+  await getUser(req, res);
+});
+
+app.get("/sessions", async (req, res) => {
+  await getLoggedInUser(req, res);
+});
+
+app.post("/sessions", async (req, res) => {
+  await startSession(req, res);
+});
+
+app.delete("/sessions", async (req, res) => {
+  await endSession(req, res);
+});
+
+app.get("/profile", async (req, res) => {
+  await getProfile(req, res);
+});
+
+app.patch("/profile/picture", async (req, res) => {
+  await updateProfilePicture(req, res);
+});
+
+app.post("/profile/recipes", async (req, res) => {
+  await saveRecipe(req, res);
+});
+
+app.delete("/profile/recipes", async (req, res) => {
+  await deleteSavedRecipe(req, res);
+});
+
+app.post("/recipe", async (req, res) => {
+  await createRecipe(req, res);
+});
+
+app.get("/recipe", async (req, res) => {
+  await getRecipe(req, res);
+});
+
+app.get("/cuisines", async (req, res) => {
+  await getCuisines(req, res);
+});
+
+app.get("/diets", async (req, res) => {
+  await getDiets(req, res);
+});
+
+// DISCUSS THIS BECOS AM CONFUSED, what should all my endpoints be? get recipe for each individual recipe displayed
+// and get cuisines to populate cuisines drop down etc.. but what else should i be getting
