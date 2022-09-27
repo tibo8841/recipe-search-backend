ee24c71f0f5e048a1b837351d3e1d06191f340bb
tibo8841 <thibaut.hucker@gmail.com>
2022-09-27 19:02:09 +0100

    .

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
 
