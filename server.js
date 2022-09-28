c4d1afe2822db97ba64cc68e541de520b5f63ef6
tibo8841 <thibaut.hucker@gmail.com>
2022-09-28 15:52:15 +0100

    no more endpoints to test

--- server.js
+++ server.js
@@ -37,73 +37,73 @@ app.get("/", (req, res) => {
   res.send("Recipe Search is working! see github repo for other endpoints");
 });
 
-app.post("/register", async (req, res) => {
-  await registerUser(req, res);
-});
+// app.post("/register", async (req, res) => {
+//   await registerUser(req, res);
+// });
 
-app.get("/login", async (req, res) => {
-  await getUser(req, res);
-});
+// app.get("/login", async (req, res) => {
+//   await getUser(req, res);
+// });
 
-app.get("/sessions", async (req, res) => {
-  await getLoggedInUser(req, res);
-});
+// app.get("/sessions", async (req, res) => {
+//   await getLoggedInUser(req, res);
+// });
 
-app.post("/sessions", async (req, res) => {
-  await startSession(req, res);
-});
+// app.post("/sessions", async (req, res) => {
+//   await startSession(req, res);
+// });
 
-app.delete("/sessions", async (req, res) => {
-  await endSession(req, res);
-});
+// app.delete("/sessions", async (req, res) => {
+//   await endSession(req, res);
+// });
 
-app.get("/profile", async (req, res) => {
-  await getProfile(req, res);
-});
+// app.get("/profile", async (req, res) => {
+//   await getProfile(req, res);
+// });
 
-app.patch("/profile/picture", async (req, res) => {
-  await updateProfilePicture(req, res);
-});
+// app.patch("/profile/picture", async (req, res) => {
+//   await updateProfilePicture(req, res);
+// });
 
-app.post("/profile/recipes", async (req, res) => {
-  await saveRecipe(req, res);
-});
+// app.post("/profile/recipes", async (req, res) => {
+//   await saveRecipe(req, res);
+// });
 
-app.delete("/profile/recipes", async (req, res) => {
-  await deleteSavedRecipe(req, res);
-});
+// app.delete("/profile/recipes", async (req, res) => {
+//   await deleteSavedRecipe(req, res);
+// });
 
-app.post("/recipe", async (req, res) => {
-  await createRecipe(req, res);
-});
+// app.post("/recipe", async (req, res) => {
+//   await createRecipe(req, res);
+// });
 
-app.get("/recipe", async (req, res) => {
-  await getRecipe(req, res);
-});
+// app.get("/recipe", async (req, res) => {
+//   await getRecipe(req, res);
+// });
 
-app.get("/cuisines", async (req, res) => {
-  getCuisines.getCuisines(req, res, client);
-});
+// app.get("/cuisines", async (req, res) => {
+//   getCuisines.getCuisines(req, res, client);
+// });
 
-app.get("/diets", async (req, res) => {
-  getDiets.getDiets(req, res, client);
-});
+// app.get("/diets", async (req, res) => {
+//   getDiets.getDiets(req, res, client);
+// });
 
-app.get("/ingredients", async (req, res) => {
-  getIngredients.getIngredients(req, res, client);
-});
+// app.get("/ingredients", async (req, res) => {
+//   getIngredients.getIngredients(req, res, client);
+// });
 
-app.post("/ingredients", async (req, res) => {
-  addIngredients.addIngredients(req, res, client);
-});
+// app.post("/ingredients", async (req, res) => {
+//   addIngredients.addIngredients(req, res, client);
+// });
 
-app.post("/cuisines", async (req, res) => {
-  addCuisines.addCuisines(req, res, client);
-});
+// app.post("/cuisines", async (req, res) => {
+//   addCuisines.addCuisines(req, res, client);
+// });
 
-app.post("/diets", async (req, res) => {
-  addDiets.addDiets(req, res, client);
-});
+// app.post("/diets", async (req, res) => {
+//   addDiets.addDiets(req, res, client);
+// });
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Example app listening on port ${PORT}`);
