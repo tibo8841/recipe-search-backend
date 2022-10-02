cb8dad91561ec5a66ea1deeef43987383aad9c93
tibo8841 <thibaut.hucker@gmail.com>
2022-10-02 20:00:00 +0100

    starting create recipe endpoint

--- createRecipe.js
+++ createRecipe.js
@@ -9,11 +9,31 @@ async function createRecipe(req, res, client) {
     diets,
   } = req.body;
 
-  // ingredients.forEach((ingredient) => {
-  //   client.query(`INSERT INTO ingredients (ingredient_name) VALUES ($1)`, [
-  //     ingredient,
-  //   ]);
-  // });
+  console.log(
+    recipeName,
+    recipeLink,
+    imageLink,
+    minsTaken,
+    ingredients,
+    cuisines,
+    diets
+  );
+
+  client.query(
+    `INSERT INTO recipes (name, picture, link, mins_taken) VALUES ($1, $2, $3, $4)`,
+    [recipeName, imageLink, recipeLink, minsTaken]
+  );
+
+  const recipeID = client.query(`SELECT id FROM recipes WHERE name = '$1'`, [
+    recipeName,
+  ]);
+
+  ingredients.forEach((ingredient) => {
+    client.query(
+      `INSERT INTO recipes_ingredients (recipe_id, ingredient_id) VALUES ($1, $2)`,
+      [recipeID, ingredient[id]]
+    );
+  });
 
   res.json({ response: "Recipe added" });
 }
